3c8b5e9b84462c1ca16bf92a0fd06f632f0be322
rajeswarioruganti <99074971+rajeswarioruganti@users.noreply.github.com>
2022-05-07 15:08:29 +0200

    Assignment2_DSA_Submission

---
+++ src/Query1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Query1 {
+	public static void main(String[] args) 
+	{		
+		System.out.println("Enter the total no. of floors in the building:");
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		int x[] = new int[n+1];
+		for(int i=1;i<n+1;i++)
+		{
+			System.out.println("Enter the floor size given on day:" +i);	
+			int m=sc.nextInt();
+			x[m]=i;
+		}
+			int j=n;
+			boolean flag;
+			System.out.println("The order of the construction is as follows:");
+			for(int i=1;i<=n;i++)
+			{
+				flag=false;
+				System.out.println("Day "+i+" :");
+				while(j>=1 && x[j]<=i)
+				{
+					flag=true;
+					System.out.println(j +" ");
+					j--;
+				}
+				if(flag==true)
+				{
+					System.out.println();
+					
+				}
+			}
+			sc.close();
+		}
+}
